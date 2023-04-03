d6f2a86aaf499dc8b209b328c7e399389dc05a0a
Jean-Francis Bastien <jfbastien@petalmd.com>
2023-04-03 12:39:07 -0400

    puts

--- main.rb
+++ main.rb
@@ -9,5 +9,7 @@ github_service.download_gemfiles
 `gem install bundler -v #{github_service.bundler_version}`
 
 outputs = `bundle _#{github_service.bundler_version}_ outdated --strict`
+pp outputs
 parsing = Parsing.new(outputs)
+pp parsing.to_md
 github_service.update_the_issue(parsing.gems_explicit.size, parsing.to_md)
