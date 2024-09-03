a2a5b9ee6a8328b46a124689c78ae9de101e09bf
Jean-Francis Bastien <jfbastien@petalmd.com>
2024-09-03 11:43:03 -0400

    fix

--- parsing.rb
+++ parsing.rb
@@ -69,7 +69,8 @@ class Parsing
   end
 
   def badge_link
-    workflow_file = ENV['GITHUB_WORKFLOW'].split('/').detect { |file| file.match?(/\.yml$/) }
+    puts ENV['GITHUB_WORKFLOW']
+    workflow_file = ENV['GITHUB_WORKFLOW'].split('/').detect { |path| path.include?('.yml') }
     # uri = CGI.escape("https://github.com/#{ENV['GEMFILE_REPOSITORY']}/actions/workflows/#{ENV['GITHUB_WORKFLOW']}/badge.svg")
     # "![#{ENV['GITHUB_WORKFLOW']}](#{uri})"
     "[![Outdated Gems](https://github.com/#{ENV['GEMFILE_REPOSITORY']}/actions/workflows/#{workflow_file}/badge.svg)](https://github.com/#{ENV['GEMFILE_REPOSITORY']}/actions/workflows/#{workflow_file})"
